8629d950b8350fac8412b9be2ff22b412a233a7d
meblo98 <lomouhamedelbachir@gmail.com>
2025-05-19 09:37:43 +0000

    build pour le deploiement

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import Baniere from '@/components/Baniere.vue';
-import NosSolutions from '@/components/NosSolutions.vue';
-import Cta from '@/components/Cta.vue';
-import ValueProps from '@/components/ValueProps.vue';
-import ProductShowcase from '@/components/ProductShowcase.vue';
+// import NosSolutions from '@/components/NosSolutions.vue';
+// import Cta from '@/components/Cta.vue';
+// import ValueProps from '@/components/ValueProps.vue';
+// import ProductShowcase from '@/components/ProductShowcase.vue';
 import Contact from '@/components/Contact.vue';
 import CeQueNousProposons from '@/components/CeQueNousProposons.vue';
 import Apropos from '@/components/Apropos.vue';
